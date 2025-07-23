66a109efdb7f8cb5485a66174c7bfbece00dd1b3
kimmisung <kayaa65@naver.com>
2025-07-24 01:00:15 +0900

    [Anonymous class, Lambda,Functional Interface]

---
+++ src/main/java/functionalinterface/thread/RunnableExample.java
@@ -0,0 +1,10 @@
+package functionalinterface.thread;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        Runnable task = () -> System.out.println("Hello, World!");
+
+        Thread thread = new Thread(task);
+        thread.start();
+    }
+}
