9c9647e6f5f048563c2f5f93f1fb0e646262007b
kimmisung <kayaa65@naver.com>
2025-07-25 04:12:00 +0900

    [Java Version의 변화] Record

---
+++ src/main/java/javachange/java8/interpacedefaultmethod/java17/record/PersonClass.java
@@ -0,0 +1,19 @@
+package javachange.java8.interpacedefaultmethod.java17.record;
+
+public class PersonClass {
+    private final String name;
+    private final int age;
+
+    public PersonClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/javachange/java8/interpacedefaultmethod/java17/record/PersonRecord.java
@@ -0,0 +1,7 @@
+package javachange.java8.interpacedefaultmethod.java17.record;
+
+public record PersonRecord (
+    String name,
+    int age
+) {
+}
